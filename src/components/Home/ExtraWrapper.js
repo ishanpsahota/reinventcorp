2e785992b18823eec2304e93ecc6fef0f8d31c9c
Ishan Prasad <ishanprasad.sahota@gmail.com>
2022-09-15 22:03:53 -0700

    team wrapper done

--- src/components/Home/ExtraWrapper.js
+++ src/components/Home/ExtraWrapper.js
@@ -1,10 +1,10 @@
 
-import sideImg from '../../assets/images/home-extra-one.png';
+// import sideImg from '../../assets/images/home-extra-one.png';
 
 function ExtraWrapper() {
     return (
         <div className="home-extra-wrapper">
-            <div className="home-extra-wrapper-one">
+            {/* <div className="home-extra-wrapper-one">
                 <div className='home-extra-one-img d-none d-md-block'>
                     <img src={sideImg} alt='side_image' />
                 </div>
@@ -16,9 +16,18 @@ function ExtraWrapper() {
 
                     </div>
                 </div>
-            </div>
+            </div> */}
             <div className="home-extra-wrapper-two mt-auto mb-0">
-
+                <div className="bg-glass">
+                    <div className='two-text'>
+                        <div className='circle-wrapper'>
+                            <h1> Text Here</h1>
+                        </div>
+                        <div className='text'>
+                            Just put some text here
+                        </div>
+                    </div>
+                </div>                
             </div>
         </div>
     );
